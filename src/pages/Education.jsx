2b82d7604584421baea6811ae9f70ebd0289e233
Ansh Jain <anshjain0503@gmail.com>
2026-03-15 22:44:12 +0000

    Riverside Photo Only

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -56,7 +56,8 @@ const resources = [
     icon: BookOpen,
     name: 'Chop Suey Books',
     description: 'A beloved independent bookstore in Carytown offering new, used, and rare books for all ages.',
-    link: '#'
+    link: '#',
+    image: 'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69642c930690d6cb4d4ad04d/33d599d2a_image.png'
   },
   {
     icon: Building,
