23797ebaba5985e5ee5e17fe57c874224f55922d
Ansh Jain <anshjain0503@gmail.com>
2026-03-15 22:55:31 +0000

    File changes

--- src/pages/VirtualReality.jsx
+++ src/pages/VirtualReality.jsx
@@ -24,7 +24,7 @@ const virtualWorlds = [
     id: 3,
     name: 'Virginia State Capitol',
     description: 'Tour the historic seat of Virginia\'s government designed by Thomas Jefferson',
-    image: 'https://images.unsplash.com/20/cambridge.JPG?w=800',
+    image: 'https://media.base44.com/images/public/69642c930690d6cb4d4ad04d/300ec980f_image.png',
     embedUrl: 'https://www.google.com/maps/embed?pb=!4v1705948323456!6m8!1m7!1sCAoSLEFGMVFpcE1wTWY0WmpfZEh0ckc0LURlNnBGWGFRNmlhQXZpaGhNYUxwNXdV!2m2!1d37.53858!2d-77.43403!3f0!4f0!5f0.7820865974627469',
     category: 'Landmark'
   },
